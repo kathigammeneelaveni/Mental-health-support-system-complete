b8f22b26adfba7eab362b4bedf53a660ea36024e
kathigammeneelaveni <kathigammeneelaveni@gmail.com>
2025-05-14 17:02:48 +0530

    Initial commit: frontend and backend added

---
+++ mental-health-backend/src/main/java/com/mentalhealth/mental_health_backend/MentalHealthBackendApplication.java
@@ -0,0 +1,14 @@
+package com.mentalhealth.mental_health_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthBackendApplication.class, args);
+		System.out.println("SPring boot backend is running");
+	}
+
+}
